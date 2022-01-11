e8e49ac21c72d45a4bf78d7a28361d7ad6fa97fe
Owen Agius <73174341+Fliinko@users.noreply.github.com>
2022-01-11 11:24:42 +0100

    SVM Trained

--- hogSVM.py
+++ hogSVM.py
@@ -37,7 +37,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                                   pixels_per_cell=(pix_per_cell, pix_per_cell),
                                   cells_per_block=(cell_per_block, cell_per_block),
                                   transform_sqrt=False,
-                                  visualise=vis, feature_vector=feature_vec)
+                                  visualize=vis, feature_vector=feature_vec)
         return features, hog_image
     # Otherwise call with one output
     else:
@@ -45,7 +45,7 @@ def get_hog_features(img, orient, pix_per_cell, cell_per_block,
                        pixels_per_cell=(pix_per_cell, pix_per_cell),
                        cells_per_block=(cell_per_block, cell_per_block),
                        transform_sqrt=False,
-                       visualise=vis, feature_vector=feature_vec)
+                       visualize=vis, feature_vector=feature_vec)
         return features
 
 # Spatial features of image
@@ -122,3 +122,65 @@ def extract_features(imgs, color_space='RGB', spatial_size=(32, 32),
         features.append(np.concatenate(file_features))
     # Return list of feature vectors
     return features
+
+
+vehicles = glob.glob('F:/data/vehicles/*.png')
+nonvehicles = glob.glob('F:/data/non-vehicles/*.png')
+
+print("Number of Vehicles in Dataset: ", len(vehicles))
+print("Number of Non-Vehicles in Dataset:",len(nonvehicles))
+
+color_space = 'RGB2YCrCb' # Can be RGB, RGB2HSV, RGB2LUV, RGB2HLS, RGB2YUV, RGB2YCrCb, RGB2Lab
+orient = 9  # HOG orientations
+pix_per_cell = 8 # HOG pixels per cell
+cell_per_block = 2 # HOG cells per block
+hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
+spatial_size = (32, 32) # Spatial binning dimensions, rank: 32 >= 16
+hist_bins = 64    # Number of histogram bins
+hist_range=(0, 256)
+spatial_feat = True # Spatial features on or off
+hist_feat = True # Histogram features on or off
+hog_feat = True # HOG features on or off
+
+features_v = extract_features(vehicles, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins,
+                        orient=orient, pix_per_cell=pix_per_cell,
+                        cell_per_block=cell_per_block,
+                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+features_nv = extract_features(nonvehicles, color_space=color_space,
+                        spatial_size=spatial_size, hist_bins=hist_bins,
+                        orient=orient, pix_per_cell=pix_per_cell,
+                        cell_per_block=cell_per_block,
+                        hog_channel=hog_channel, spatial_feat=spatial_feat,
+                        hist_feat=hist_feat, hog_feat=hog_feat)
+
+X = np.vstack((features_v, features_nv)).astype(np.float64)
+
+X_scaler = StandardScaler().fit(X)
+scaled_X = X_scaler.transform(X)
+
+# Define the labels vector
+y = np.hstack((np.ones(len(features_v)), np.zeros(len(features_nv))))
+
+
+# Split up data into randomized training and test sets
+rand_state = np.random.randint(0, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    scaled_X, y, test_size=0.2, random_state=rand_state)
+
+print('Using:', orient, 'orientations', pix_per_cell, 'pixels per cell and', cell_per_block, 'cells per block')
+print('Feature vector length:', len(X_train[0]))
+
+svc = LinearSVC()
+start = time.time()
+
+svc.fit(X_train, y_train)
+end = time.time()
+print(round(end-start, 2), 'Seconds to train SVC...')
+
+print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+t = time.time()
+
+#Vehicle Detection --------------
+#HOG Subsampling is used to search for vehicles in the image
